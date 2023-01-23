a7a2936a33d651ce59cce937b2ef8256fd940080
Nadasaidi2002 <118190251+Nadasaidi2002@users.noreply.github.com>
2023-01-23 21:41:29 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+console.log("hi");
+
+var menuknop = document.querySelector("header button");
+
+menuknop.addEventListener("click", menuopenen);
+
+function menuopenen() {
+  var menulist = document.querySelector("header nav");
+  menulist.classList.toggle("open")
+}
+
+var deMenuButton = document.querySelector("header button");
+
+deMenuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  deMenuButton.classList.toggle("open");
+}
